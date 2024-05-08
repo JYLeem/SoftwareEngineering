a46cdc10faf9eb969641983d2239066b8d0746ea
박진영 <am030813@naver.com>
2024-05-08 19:28:20 +0900

    Fix: DB 연결, 페이지 전환 코드 간소화

--- AdminUserManagePage.py
+++ AdminUserManagePage.py
@@ -4,26 +4,18 @@ from tkinter import Tk, Canvas, messagebox
 import tkinter as tk
 import subprocess
 import sys
-def ConnectMysql():
-    try:
-        connection = mysql.connector.connect(host='ystdb.cl260eouqhjz.ap-northeast-2.rds.amazonaws.com',
-                                                database='wordbook',
-                                                user='admin',
-                                                password='seat0323')
-        if connection.is_connected():
-            return connection
-    except Error as e:
-            print("Error while connecting to MySQL", e)  
+from Util import Util
+
+connection = Util.ConnectMysql()
 
 def ListEntireUsers(window):
     # 기존 버튼 제거
     for UserButton in UserButtons:
         UserButton.destroy()
     UserButtons.clear()
-
+    
     gap = 30
     yBefore = 29
-    connection = ConnectMysql()
     if connection:
         cursor = connection.cursor()
         cursor.execute("SELECT nickname FROM user")  # id만 가져오도록 수정
@@ -41,15 +33,12 @@ def ListSpecificUsers(window):
     if(len(SearchUser) ==0):
         messagebox.showinfo("검색 오류", "입력이 올바르게 되지 않았습니다. 다시 시도해주세요.")
     else:
-        
         for UserButton in UserButtons:
             UserButton.destroy()
         UserButtons.clear()
 
-
         gap = 30
         yBefore = 29
-        connection = ConnectMysql()
         if connection:
             cursor = connection.cursor()
             cursor.execute("SELECT nickname FROM user WHERE nickname LIKE %s", ('%' + SearchUser + '%',))
@@ -66,15 +55,12 @@ def ListSpecificUsers(window):
 
 # 초기화
 UserButtons = []
-def GoAdminStatusPage(adminID):
-    window.destroy()
-    subprocess.run(['python', 'AdminStatusPage.py',adminID])
 window = Tk()
 window.title("회원 관리 페이지")
 window.geometry("747x531")
 window.configure(bg = "#FFFFFF")
 
-adminID = "pjy"
+adminID = sys.argv[1]
 canvas = Canvas(
     window,
     bg = "#FFFFFF",
@@ -106,7 +92,7 @@ TextWidget = tk.Text(window)
 scrollbar = tk.Scrollbar(window, command=TextWidget.yview)
 scrollbar.pack(side="right", fill="y")
 
-GoHomeBtn = tk.Button(text="이전으로", command=lambda:GoAdminStatusPage(adminID))
+GoHomeBtn = tk.Button(text="이전으로", command=lambda:Util.SwitchPage(window,"AdminMainPage",adminID))
 GoHomeBtn.place(x=15, y = 483, width=90, height=40)
 
 
