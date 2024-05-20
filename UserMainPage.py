68f285b226075fb5b6bed79d77545caf19e8eff3
박진영 <am030813@naver.com>
2024-05-20 11:38:30 +0900

    FIX : 클래스화, 로그아웃 추가

--- UserMainPage.py
+++ UserMainPage.py
@@ -1,38 +1,52 @@
-from tkinter import Tk, Canvas, Text, Button
+from tkinter import Tk, Canvas, Button
 import tkinter as tk
-import subprocess
-import sys  
-import time
+import sys
 from Util import Util
+from LoginPage import LoginPage
 
-connection = Util.ConnectMysql()
-window = Tk()
-window.geometry("747x531")
-window.configure(bg = "#FFFFFF")
+class UserMainPage(tk.Tk):
+    def __init__(self, userID):
+        super().__init__()
+        self.geometry("747x531")
+        self.configure(bg="#FFFFFF")
+        self.resizable(False, False)
+        self.userID = userID
+        self.connection = Util.ConnectMysql()
+        self.canvas = Canvas(
+            bg="#FFFFFF",
+            height=531,
+            width=747,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        self.canvas.place(x=0, y=0)
 
-userID = sys.argv[1]
+        
+        self.UserIDLabel = tk.Label(self, text=userID)
+        self.UserIDLabel.place(x=300, y=0, width=100, height=30)
+        
+        self.studyDayBtn = tk.Button(text="단어 학습", command=lambda: Util.SwitchPage("UserStudyDayPage", self.userID))
+        self.studyDayBtn.place(x=283, y=100, width=180, height=80)
 
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 531,
-    width = 747,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
+        self.manageUserBtn = tk.Button(text="단어 시험", command=lambda: Util.SwitchPage("UserExamDayPage", self.userID))
+        self.manageUserBtn.place(x=283, y=200, width=180, height=80)
 
-canvas.place(x = 0, y = 0)
-StudyDayBtn = tk.Button(text = "단어 학습", command=lambda:Util.SwitchPage(window, "UserStudyDayPage", userID))
-StudyDayBtn.place(x=283, y=98, width=180, height=80)
+        self.gameBtn = tk.Button(text="게임", command=lambda: Util.SwitchPage("game", self.userID))
+        self.gameBtn.place(x=283, y=300, width=180, height=80)
 
-ManageUserBtn = tk.Button(text="단어 시험")
-ManageUserBtn.place(x=283, y=231, width=180, height=80)
-
-LogOutBtn = tk.Button(text = "로그아웃")
-LogOutBtn.place(x=283, y=364, width=180, height=80)
-
-
-
-window.resizable(False, False)
-window.mainloop()
+        self.logOutBtn = tk.Button(text="로그아웃", command=self.SwitchToLoginPage)
+        self.logOutBtn.place(x=283, y=400, width=180, height=80)
+        
+    def SwitchToLoginPage(self):
+            self.cursor = self.connection.cursor()
+            self.cursor.execute("UPDATE user SET isaccess = 0 WHERE id = %s",(userID,))
+            self.connection.commit()  # 커밋을 수행하여 변경 사항을 DB에 적용
+            self.cursor.close()  # 커서 닫기 
+            self.destroy()
+            app = LoginPage(self)
+            app.mainloop()
+if __name__ == "__main__":
+    userID = sys.argv[1] if len(sys.argv) > 1 else 'default_user'
+    app = UserMainPage(userID)
+    app.mainloop()
